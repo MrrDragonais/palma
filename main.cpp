a1b85bc7c1761383792ecbc4f5afdd58d24349db
palma <Nikita.selezhev@gmail.com>
2023-02-03 08:51:55 +0400

    4. Properly string color

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+
+class Pen{
+	public:
+	std::string color;
+};
 int main() {
 	using namespace std;
 	cout << "Test v1.01:OK.\n" << endl;
 	Pen pen;
+	pen.color = "#FFFFFF"
+	cout << "pen.color = " << pen.color <<endl;
 	return 0;
 	
 	
